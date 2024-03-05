ca6246196537fa31763b6829e04ea278740cd982
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-03-05 10:48:16 +0100

    Inital commit

---
+++ ExerciseTracker/Models/Exercise.cs
@@ -0,0 +1,11 @@
+namespace ExerciseTracker;
+
+public class Exercise
+{
+    public int Id;
+    public DateTime DateStart;
+    public DateTime DateEnd;
+    public TimeSpan duration;
+    public string? comments;
+
+}
